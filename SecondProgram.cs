ca4df60929ea5ef7cdebee53d7c21c1057134611
ArpithaKBIyer <79208525+ArpithaKBIyer@users.noreply.github.com>
2021-09-29 10:17:02 +0530

    Add project files.

---
+++ SecondProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SecondProgram
+    {
+        static void Main(string[] args)
+        {
+            Acceptdata();
+            Console.ReadLine();
+        }
+
+        public static void Acceptdata()
+        {
+            Console.WriteLine("Enter a  value for i");
+            //Convert string to integer
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for j");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+        }
+    }
+}
